778a653b406959846a8636e023b02518b61e312d
Monarch Wadia <monarch@zeroprojects.ca>
2020-08-05 10:05:01 -0400

    Added webgazer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert('ok');
+webgazer.showPredictionPoints(true);
+webgazer.setRegression("ridge");
+webgazer.showPredictionPoints(true);
+
+webgazer.setGazeListener(function(data, elapsedTime) {
+  if (data == null) {
+      return;
+  }
+  var xprediction = data.x; //these x coordinates are relative to the viewport
+  var yprediction = data.y; //these y coordinates are relative to the viewport
+
+  console.log(yprediction, xprediction); //elapsed time is based on time since begin was called
+}).begin();
