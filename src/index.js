6efc31b6ec811cbd1e7f50c055ee7df82132e99d
Monarch Wadia <monarch@zeroprojects.ca>
2020-08-05 10:37:20 -0400

    Added calibration dots

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 webgazer.showPredictionPoints(true);
 webgazer.setRegression("ridge");
-webgazer.showPredictionPoints(true);
+
+window.saveDataAcrossSessions = true;
+
+function drawDot(x, y, color='blue') {
+  const dot = document.createElement('div');
+  dot.style.position = 'absolute';
+  dot.style.width = '10px';
+  dot.style.height = '10px';
+  dot.style.zIndex = '999999';
+  dot.style.borderRadius = '500px';
+  dot.style.backgroundColor = color;
+  dot.style.left = x;
+  dot.style.top = y;
+  document.body.appendChild(dot);
+}
+
+// drawing calibration dots
+const calDot = (x, y) => drawDot(x, y, 'red');
+
+calDot('50%', '0px');
+calDot('95%', '0px');
+calDot('95%', '50%');
+calDot('95%', '95%');
+calDot('50%', '95%');
+calDot('1%', '95%');
+calDot('1%', '50%');
+calDot('1%', '1%');
+calDot('50%', '50%');
 
 webgazer.setGazeListener(function(data, elapsedTime) {
   if (data == null) {
       return;
   }
-  var xprediction = data.x; //these x coordinates are relative to the viewport
-  var yprediction = data.y; //these y coordinates are relative to the viewport
 
-  console.log(yprediction, xprediction); //elapsed time is based on time since begin was called
+  drawDot(data.x + 'px', data.y + 'px');
 }).begin();
