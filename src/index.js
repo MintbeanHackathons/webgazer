5d2c8011f059e761d7d5af8f336f4ac7b854a7b5
Jimmy Peng <jimmy.peng@mintbean.io>
2020-08-05 11:09:28 -0400

    Styling changes

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ webgazer.setRegression("ridge");
 
 window.saveDataAcrossSessions = true;
 
-function drawDot(x, y, color='blue') {
-  const dot = document.createElement('div');
-  dot.style.position = 'absolute';
-  dot.style.width = '10px';
-  dot.style.height = '10px';
-  dot.style.zIndex = '999999';
-  dot.style.borderRadius = '500px';
+function drawDot(x, y, color = "blue", r = "10px") {
+  const dot = document.createElement("div");  
+  dot.style.position = "absolute";
+  dot.style.width = r;
+  dot.style.height = r;
+  dot.style.zIndex = "999999";
+  dot.style.borderRadius = "500px";
   dot.style.backgroundColor = color;
   dot.style.left = x;
   dot.style.top = y;
@@ -17,22 +17,24 @@ function drawDot(x, y, color='blue') {
 }
 
 // drawing calibration dots
-const calDot = (x, y) => drawDot(x, y, 'red');
+const calDot = (x, y) => drawDot(x, y, "red", "20px");
 
-calDot('50%', '0px');
-calDot('95%', '0px');
-calDot('95%', '50%');
-calDot('95%', '95%');
-calDot('50%', '95%');
-calDot('1%', '95%');
-calDot('1%', '50%');
-calDot('1%', '1%');
-calDot('50%', '50%');
+calDot("50%", "0px");
+calDot("95%", "0px");
+calDot("95%", "50%");
+calDot("95%", "95%");
+calDot("50%", "95%");
+calDot("1%", "95%");
+calDot("1%", "50%");
+calDot("1%", "0px");
+calDot("50%", "50%");
 
-webgazer.setGazeListener(function(data, elapsedTime) {
-  if (data == null) {
+webgazer
+  .setGazeListener(function (data, elapsedTime) {
+    if (data == null) {
       return;
-  }
+    }
 
-  drawDot(data.x + 'px', data.y + 'px');
-}).begin();
+    drawDot(data.x + "px", data.y + "px");
+  })
+  .begin();
